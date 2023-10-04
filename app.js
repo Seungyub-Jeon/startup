5ef0e5561105119037a5b97db80c65c160e6114f
seungyub <seungyub.jeon@gmail.com>
2023-10-04 17:03:37 +0900

    추가 템플릿 수정

--- app.js
+++ app.js
@@ -17,20 +17,41 @@ app.get('/', (req, res) => {
 app.get('/blog', (req, res) => {
     res.render('blog', { pageTitle: 'Blog' });
 });
+app.get('/blog/blog-page', (req, res) => {
+    res.render('blog-page', { pageTitle: 'blog-page' });
+});
 app.get('/shop', (req, res) => {
     res.render('shop', { pageTitle: 'Shop' });
+});
+app.get('/shop/product', (req, res) => {
+    res.render('product', { pageTitle: 'product' });
 });
 app.get('/groups', (req, res) => {
     res.render('groups', { pageTitle: 'Groups' });
 });
+app.get('/groups/group-profile', (req, res) => {
+    res.render('group-profile', { pageTitle: 'group-profile' });
+});
 app.get('/members', (req, res) => {
     res.render('members', { pageTitle: 'Members' });
+});
+app.get('/members/members-page', (req, res) => {
+    res.render('members-page', { pageTitle: 'members-page' });
 });
 app.get('/courses', (req, res) => {
     res.render('courses', { pageTitle: 'Courses' });
 });
+app.get('/courses/courses-page', (req, res) => {
+    res.render('courses-page', { pageTitle: 'courses-page' });
+});
 app.get('/events', (req, res) => {
     res.render('events', { pageTitle: 'Events' });
+});
+app.get('/events/event', (req, res) => {
+    res.render('event', { pageTitle: 'Event' });
+});
+app.get('/freelancers', (req, res) => {
+    res.render('freelancers', { pageTitle: 'freelancers' });
 });
 
 
